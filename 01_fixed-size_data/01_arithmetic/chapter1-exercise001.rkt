2cd69be2b3e27b51079c57eadfe868204d1df3d3
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-07-28 14:57:03 +0300

    Delete chapter1-exercise001.rkt

--- 01_fixed-size_data/01_arithmetic/chapter1-exercise001.rkt
+++
@@ -1,9 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |;chapter1-exercise1|) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
-;chapter1-exercise1
-
-(define x 3)
-(define y 4)
-
-(sqrt (+ (sqr x) (sqr y)))
