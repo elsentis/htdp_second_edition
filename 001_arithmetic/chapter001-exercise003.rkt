a20718e831b8ef4ba0d8b579aafa1e239552014d
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-07-28 15:21:17 +0300

    Add files via upload

---
+++ 001_arithmetic/chapter001-exercise003.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname chapter1-exercise003) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
+; chapter001-exercise003
+
+; A:
+; create an expression using string primitives that adds "_" at position i
+
+
+(define str "helloworld")
+(define i 5)
+
+(string-append (substring str 0 i) "_" (substring str i))
