7538ba97ac8b184d1441039b0e5787452011e21d
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-07-30 13:57:11 +0300

    Update chapter001-exercise003.rkt

--- 001_arithmetic/chapter001-exercise003.rkt
+++ 001_arithmetic/chapter001-exercise003.rkt
@@ -3,7 +3,7 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname chapter1-exercise003) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
 ; chapter001-exercise003
 
-; A:
+; Q:
 ; create an expression using string primitives that adds "_" at position i
 
 
