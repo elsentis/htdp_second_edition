abd0459bb8165c7002edb152960cb712f636b059
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-07-31 15:26:59 +0300

    Add files via upload

---
+++ 001_arithmetic/chapter001-exercise010.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname chapter001-exercise010) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
+; chapter001-exercise010
+
+; Q.:
+; Now relax, eat, sleep, and then tackle the next chapter.
+
+; ;)))
