95f3816863b2d4c039dc33265d71e297fbb8115c
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-07-31 14:15:00 +0300

    Add files via upload

---
+++ 001_arithmetic/chapter001-exercise007.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname chapter001-exercise007) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
+; chapter001-exercise007
+
+; Q.: You go to the mall either if it is not sunny or today is Friday
+;     create an expression that computes whether sunny is false or friday is true
+
+
+(define sunny #true)
+(define friday #false)
+
+(or (not sunny) friday)
