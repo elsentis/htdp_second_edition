3dbb1c309fede61d7843a3bfeb3728ccd81b3eef
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-08-01 15:59:55 +0300

    Update and rename chapter006-exercise098.rkt to exercise098-chapter006.rkt

--- 001_arithmetic/exercise098-chapter006.rkt
+++ 001_arithmetic/exercise098-chapter006.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname chapter006-exercise098) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
-; chapter006-exercise098
+; exercise098-chapter006
 
 ; Q.:
 ; Design the function si-game-over? for use as the stop-when handler.
