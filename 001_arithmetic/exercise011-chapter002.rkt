4c904be4728666bdedd4e773589c6c7d4d9c70f0
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-08-01 15:55:29 +0300

    Update and rename chapter002-exercise011.rkt to exercise011-chapter002.rkt

--- 001_arithmetic/exercise011-chapter002.rkt
+++ 001_arithmetic/exercise011-chapter002.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname chapter002-exercise011) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
-; chapter002-exercise011
+; exercise011-chapter002
 
 ; Q.:
 ; Define a function that consumes two numbers, x and y, and that computes the distance of point (x,y) to the origin
