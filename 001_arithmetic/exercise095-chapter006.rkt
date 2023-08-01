9461dacca02445409f90cffdf9f8f3d58c2493fd
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-08-01 15:58:17 +0300

    Update and rename chapter006-exercise095.rkt to exercise095-chapter006.rkt

--- 001_arithmetic/exercise095-chapter006.rkt
+++ 001_arithmetic/exercise095-chapter006.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname chapter006-exercise095) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
-;chapter006-exercise095
+; exercise095-chapter006
 
 ; Q.:
 ; Explain why the three instances are generated according to the first or second clause of the data definition.
