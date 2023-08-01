b2c7f9e13048944e04ce78b7957d32eaf9501c12
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-08-01 15:50:47 +0300

    Update and rename chapter001-exercise004.rkt to exercise004-chapter001.rkt

--- 001_arithmetic/exercise004-chapter001.rkt
+++ 001_arithmetic/exercise004-chapter001.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname chapter001-exercise004) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
-; chapter001-exercise004
+; exercise004-chapter001
 
 ; Q:
 ; create an expression using string primitives that deletes the ith position from str
