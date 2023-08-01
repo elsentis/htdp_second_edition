ab9521b8d103de04a4ab2a2684eb45c1180ff645
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-08-01 15:53:14 +0300

    Update and rename chapter001-exercise007.rkt to exercise007-chapter001.rkt

--- 001_arithmetic/exercise007-chapter001.rkt
+++ 001_arithmetic/exercise007-chapter001.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname chapter001-exercise007) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
-; chapter001-exercise007
+; exercise007-chapter001
 
 ; Q.: You go to the mall either if it is not sunny or today is Friday
 ;     create an expression that computes whether sunny is false or friday is true
