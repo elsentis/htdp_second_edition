ac5def51c895ff1d0e1c32e107918c595df40d32
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-08-01 15:48:54 +0300

    Update and rename chapter001-exercise001.rkt to exercise001-chapter001.rkt

--- 001_arithmetic/exercise001-chapter001.rkt
+++ 001_arithmetic/exercise001-chapter001.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |;chapter1-exercise1|) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
-; chapter001-exercise001
+; exercise001-chapter001
 
 ; Q:
 ; Write down an expression that computes the distance of this point to the origin, that is, a point with the coordinates (0,0).
