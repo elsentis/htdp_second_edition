1e3582e8f330ab8ec19608790ae809fc91fc088c
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-08-03 17:47:26 +0300

    Add files via upload

---
+++ 001_arithmetic/exercise092-chapter005.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise092-chapter005) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
+; exercise092-chapter005
+
+; Q.:
+; Design the cham program, which has the chameleon continuously walking across the canvas from left to right.
+; When it reaches the right end of the canvas, it disappears and immediately reappears on the left
+; For managing the chameleon’s happiness gauge, you may reuse the happiness gauge from the virtual cat
+
+
+
