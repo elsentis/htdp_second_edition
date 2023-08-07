585bb8d03abe640410eb029fbb38109fe755a3c5
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-08-07 22:44:28 +0300

    Add files via upload

---
+++ 001_arithmetic/exercise015-chapter002.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise015-chapter002) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; exercise015-chapter002
+
+; Q.:
+; Define ==>. The function consumes two Boolean values, call them sunny and friday.
+; Its answer is #true if sunny is false or friday is true.
+
+
+(define (==> sunny friday)
+  (if friday #t (not sunny)))
