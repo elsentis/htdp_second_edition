e2169e23fa4574b8efec27310ce0d1cbe10157ee
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-08-08 13:45:34 +0300

    Add files via upload

---
+++ 001_arithmetic/exercise024-chapter002.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise024-chapter002) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
+; exercise024-chapter002
+
+; Q.:
+; Here is the definition of ==>
+; Use the stepper to determine the value of (==> #true #false)
+
+(define (==> x y)
+  (or (not x) y))
+
+
+(==> #true #false)
+; #false
