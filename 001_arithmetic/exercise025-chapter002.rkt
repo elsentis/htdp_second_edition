7e1fae58ed43df399e9cd6f9b3ec0782c583346d
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-08-08 14:00:51 +0300

    Add files via upload

---
+++ 001_arithmetic/exercise025-chapter002.rkt
@@ -0,0 +1,20 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise025-chapter002) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
+; exercise025-chapter002
+
+; Q.:
+; Take a look at this attempt to solve exercise 17
+; Does stepping through an application suggest a fix?
+
+(define (image-classify img)
+  (cond
+    [(>= (image-height img) (image-width img)) "tall"]
+    [(= (image-height img) (image-width img)) "square"]
+    [(<= (image-height img) (image-width img)) "wide"]))
+
+
+(image-classify (rectangle 10 10 "outline" "red"))
+
+; condition of equal sides to check "tall" and "wide" is wrong
+; condition of equal sides only needed for "square" check
