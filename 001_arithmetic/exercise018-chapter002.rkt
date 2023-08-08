8f446ef0dc4aba3af77baa918bbbb4a142f3aefa
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-08-08 10:22:38 +0300

    Add files via upload

---
+++ 001_arithmetic/exercise018-chapter002.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise018-chapter002) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
+; exercise018-chapter002
+
+; Q.:
+; Define the function string-join, which consumes two strings and appends them with "_" in between. See exercise 2 for ideas.
+
+(define (string-join str1 str2)
+  (string-append str1 "_" str2))
