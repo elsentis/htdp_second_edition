f7aaa1af9d57f36681decc6f2e25e0b3adcba28c
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-08-09 08:21:49 +0300

    Add files via upload

--- 001_arithmetic/exercise113-chapter006.rkt
+++ 001_arithmetic/exercise113-chapter006.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise113-chapter006) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise113-chapter006) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
 ; exercise113-chapter006
 
 ; Q.:
@@ -57,5 +57,62 @@
 ; – a Posn
 ; interpretation an ordinary Cartesian point
 
+;(check-expect (coordinate? "lol") #false)
+;(check-expect (coordinate? 1) #true)
+;(check-expect (coordinate? (make-posn 1 1)) #true)
+;(check-expect (coordinate? (make-aim (make-posn 10 10) (make-tank 10 -3))) #false)
+
 (define (coordinate? v)
   (or (number? v) (posn? v)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+; predicate for the VAnimal data defenition
+
+; H is a Number between [0..1000].
+; interpretation represents a happiness value
+
+; X-coord is a Number
+; interpretation represents a object location coordinate
+
+; Dir is a Strings as two values
+; "left", "right"
+; interpretation represents a path direction
+
+; Color is a Strings as three values
+; "red", "green", "blue"
+; interpretation represents a color
+
+(define-struct vcat (x hap dir))
+;; A VCat is a structure:
+;;   (make-vcat Image Track)
+;; interp. a virtual cat, including 
+;;         information about its movement and happiness and path direction
+;;         x is the X-coord of the cat on the screen
+;;         hap is its happiness H
+;;         dir is a path direction
+
+
+(define-struct vcham (x hap dir color))
+;; A VCham is a structure:
+;;   (make-track X-coord H Dir)
+; interp. a virtual cham
+;; interp. keeps track of a cham's changing variables
+;;         x is the x-coordinate of the cham on the screen
+;;         hap is its happiness
+;;         dir is a path direction
+;;         color is a current color of cham
+
+; A VAnimal is either
+; – a VCat
+; – a VCham
+
+;(check-expect (vanimal? (make-vcat 120 90 "right")) #true)
+;(check-expect (vanimal? (make-vcham 100 150 "right" "green")) #true)
+;(check-expect (vanimal? (make-posn 10 10)) #false)
+;(check-expect (vanimal? 1) #false)
+;(check-expect (vanimal? "lol") #false)
+
+(define (vanimal? v)
+  (or (vcat? v) (vcham? v)))
