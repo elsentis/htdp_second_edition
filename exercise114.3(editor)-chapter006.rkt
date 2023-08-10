a50dd11d4bd5668931eea4ff62b27a6318d6777d
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-08-10 10:33:16 +0300

    Delete exercise114.3(editor)-chapter006.rkt

--- exercise114.3(editor)-chapter006.rkt
+++
@@ -1,8 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |exercise114.3(editor)-chapter006|) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
-; exercise114.3(editor)-chapter006
-
-; Q.:
-; Use the predicates from exercise 113 to check the editor program (A Graphical Editor).
-
