2b48fe5060dffb3f976958cc2e420df5d6b97dad
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-08-11 16:27:31 +0300

    Add files via upload

---
+++ 001_arithmetic/exercise032-chapter002.rkt
@@ -0,0 +1,23 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise032-chapter002) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+; exercise032-chapter002
+
+; Q.:
+; Most people no longer use desktop computers just to run applications but also employ cell phones, tablets, and their cars’ information control screen. Soon people will use wearable computers in the form of intelligent glasses, clothes, and sports gear.
+; In the somewhat more distant future, people may come with built-in bio computers that directly interact with body functions. Think of ten different forms of events that software applications on such computers will have to deal with.
+
+
+
+; 1 medical indicators
+; 1.1 arterial pressure
+; 1.2 walking speed/running speed
+; 1.3 pulse
+; 1.4 blood composition
+; 1.5 digital hearing aid
+; 1.6 vision enhancement devices
+; 1.7 reading brain impulses to interact with the environment
+; 1.8 speech apparatus
+; 1.9 control of ultraviolet radiation from the sun
+; 1.10 control of the gastrointestinal tract
+; 2 face recognition in intelligent glasses
