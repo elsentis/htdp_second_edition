c68b939856d863fafce0e71e14d922ee60dfe892
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-08-14 10:29:21 +0300

    Add files via upload

---
+++ 001_arithmetic/exercise033-chapter003.rkt
@@ -0,0 +1,27 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise033-chapter003) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; exercise033-chapter003
+
+; Q.:
+; Research the “year 2000” problem.
+
+; https://en.wikipedia.org/wiki/Year_2000_problem
+; The year 2000 problem, also commonly known as Y2K problem, Y2K scare, millennium bug, Y2K bug, Y2K glitch, Y2K error,
+; or simply Y2K refers to potential computer errors related to the formatting and storage of calendar data for dates in
+; and after the year 2000. Many programs represented four-digit years with only the final two digits, making the year 2000
+; indistinguishable from 1900. Computer systems' inability to distinguish dates correctly had the potential to bring down
+; worldwide infrastructures for computer reliant industries.
+
+; In the years leading up to the turn of the century (millennium), the public gradually became aware of the "Y2K scare",
+; and individual companies predicted the global damage caused by the bug would require anything between
+; $400 million and $600 billion to rectify. A lack of clarity regarding the potential dangers of the bug led some to stock up
+; on food, water, and firearms, purchase backup generators, and withdraw large sums of money in anticipation
+; of a computer-induced apocalypse.[2]
+
+; Contrary to published expectations, few major errors occurred in 2000. Supporters of the Y2K remediation effort argued that
+; this was primarily due to the pre-emptive action of many computer programmers and information technology experts.
+; Companies and organizations in some countries, but not all, had checked, fixed, and upgraded their computer systems to
+; address the problem. Then-U.S. president Bill Clinton, who organized efforts to minimize the damage in the United States,
+; labeled Y2K as "the first challenge of the 21st century successfully met",
+; and retrospectives on the event typically commend the programmers who worked to avert the anticipated disaster.
