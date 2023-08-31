cd155f40e270a6f1afa065bab9990ecdc4d01194
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-08-31 17:47:03 +0300

    Add files via upload

---
+++ 001_arithmetic/exercise059-chapter004.rkt
@@ -0,0 +1,72 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise059-chapter004) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+; exercise059-chapter004
+
+; Q.:
+; Finish the design of a world program that simulates the traffic light FSA
+; The function’s argument is the initial state for the big-bang expression, which tells DrRacket to redraw the state of the world with tl-render and to handle clock ticks with tl-next.
+; Complete the design of tl-render and tl-next. Complete the design of tl-render and tl-next.
+; If you decide to create images with the functions from the 2htdp/image teachpack, design an auxiliary function for creating the image of a one-color bulb.
+
+
+(define WIDTH 90)
+(define HEIGHT 30)
+(define BACKGROUND (empty-scene WIDTH HEIGHT))
+
+(define BULB-RADIUS (/ WIDTH 10))
+
+
+; A TrafficLight is one of the following Strings:
+; – "red"
+; – "green"
+; – "yellow"
+; - "gold"
+; interpretation the three strings represent the three 
+; possible states that a traffic light may assume 
+; (me add in traffic light second yellow (gold) signal, to warn of a change from red to green)
+
+
+; TrafficLight -> TrafficLight
+; simulates a clock-based American traffic light
+
+(define (traffic-light-simulation initial-state)
+  (big-bang initial-state
+    [to-draw tl-render]
+    [on-tick tl-next 1]))
+
+
+; TrafficLight -> TrafficLight
+; yields the next state, given current state cs
+
+;(check-expect (tl-next "red") "gold")
+;(check-expect (tl-next "gold") "green")
+;(check-expect (tl-next "green") "yellow")
+;(check-expect (tl-next "yellow") "red")
+
+(define (tl-next cs)
+  (cond [(string=? cs "red") "gold"]
+        [(string=? cs "gold") "green"]
+        [(string=? cs "green") "yellow"]
+        [(string=? cs "yellow") "red"]))
+
+
+
+; TrafficLight -> Image
+; renders the current state cs as an image
+
+;(check-expect (tl-render "red") (place-image (circle BULB-RADIUS "solid" "red") (/ WIDTH 8) (/ HEIGHT 2)
+;                                             (place-image (circle BULB-RADIUS "outline" "gold") (* (/ WIDTH 8) 3) (/ HEIGHT 2)
+;                                                          (place-image (circle BULB-RADIUS "outline" "yellow") (* (/ WIDTH 8) 5) (/ HEIGHT 2)
+;                                                                       (place-image (circle BULB-RADIUS "outline" "green") (* (/ WIDTH 8) 7) (/ HEIGHT 2) BACKGROUND)))))
+
+;(check-expect (tl-render "yellow") (place-image (circle BULB-RADIUS "outline" "red") (/ WIDTH 8) (/ HEIGHT 2)
+;                                             (place-image (circle BULB-RADIUS "outline" "gold") (* (/ WIDTH 8) 3) (/ HEIGHT 2)
+;                                                          (place-image (circle BULB-RADIUS "solid" "yellow") (* (/ WIDTH 8) 5) (/ HEIGHT 2)
+;                                                                       (place-image (circle BULB-RADIUS "outline" "green") (* (/ WIDTH 8) 7) (/ HEIGHT 2) BACKGROUND)))))
+
+(define (tl-render current-state)
+  (place-image (circle BULB-RADIUS (if (string=? current-state "red") "solid" "outline") "red") (/ WIDTH 8) (/ HEIGHT 2)
+               (place-image (circle BULB-RADIUS (if (string=? current-state "gold") "solid" "outline") "gold") (* (/ WIDTH 8) 3) (/ HEIGHT 2)
+                            (place-image (circle BULB-RADIUS (if (string=? current-state "yellow") "solid" "outline") "yellow") (* (/ WIDTH 8) 5) (/ HEIGHT 2)
+                                         (place-image (circle BULB-RADIUS (if (string=? current-state "green") "solid" "outline") "green") (* (/ WIDTH 8) 7) (/ HEIGHT 2) BACKGROUND)))))
