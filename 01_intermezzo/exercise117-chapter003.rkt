681bc7baf264d0b9e82f61016f1d0919c096d6a2
lestinsel <134290921+lestinsel@users.noreply.github.com>
2023-10-17 13:12:49 +0300

    Update exercise117-chapter003.rkt

--- 01_intermezzo/exercise117-chapter003.rkt
+++ 01_intermezzo/exercise117-chapter003.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise117-chapter003) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
-; exercise117-chapter003
+; exercise117-intermezzo01
 
 ; Q.:
 ; Consider the following sentences
