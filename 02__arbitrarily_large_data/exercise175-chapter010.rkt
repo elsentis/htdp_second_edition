74c71f3c41a1f82167fa59e094a098071215a48f
unknown <134290921+lestinsel@users.noreply.github.com>
2024-02-24 23:30:39 +0300

    ex.175

--- 02__arbitrarily_large_data/exercise175-chapter010.rkt
+++ 02__arbitrarily_large_data/exercise175-chapter010.rkt
@@ -22,11 +22,11 @@
 ; – (cons Los LN)
 ; interpretation a list of lines, each is a list of Strings
 
-(define-struct file-info [symbol-c words-c lines-c])
-; A file-info is a structure
-; (make-file-info Number Number Number)
+(define-struct file-info-struct [symbol-c words-c lines-c])
+; A file-info-struct is a structure
+; (make-file-info-struct Number Number Number)
 ; interpretation a set of values describing the contents of a some file:
-; count the number of 1Strings, words, and lines
+; count the number of 1Strings (with line engings and spaces), words, and lines
 
 (define line0 (cons "a" (cons "w" '())))
 (define line1 '())
@@ -39,46 +39,110 @@
 (define lls4 (cons line3 (cons line0 '())))
 
 
+; String (name of file) -> file-info
+; The purpose of the function consumes the name of a file and produces a value
+; that consists of three numbers
+; count the number of 1Strings, words, and lines in a given file
+;
+(define (file-info f)
+  (aux-file-info (read-words/line f)))
+
+
+; LN -> file-info-struct
+; consume a data from input file processed by the function read-words/line
+; and produce a file-info-struct with data about file
+;
+;(check-expect (aux-file-info lls0) (make-file-info-struct 4 2 2))
+;
+(define (aux-file-info lls)
+  (make-file-info-struct (aux-file-info-1string-count lls)
+                         (aux-file-info-word-count lls)
+                         (aux-file-info-line-count lls)))
+
+
 ; LN -> Number
 ; func for counting the number of 1Strings
 ; in inputuing LN
 ;
-;(check-expect (aux-file-info lls0) 5)
-;(check-expect (aux-file-info lls1) 11)
+;(check-expect (aux-file-info-1string-count lls0) 5)
+;(check-expect (aux-file-info-1string-count lls1) 11)
 ;
-(define (aux-file-info lls)
+(define (aux-file-info-1string-count lls)
   (cond
-    [(empty? lls) 1]
+    [(empty? lls) 0]
     [else
-     (+ (aux-line-info (first lls) 0)
+     (+ (aux-file-info-1string-count-line (first lls) 0)
         (if (empty? (rest lls)) 0 1)
-        (aux-file-info (rest lls)))]))
+        (aux-file-info-1string-count (rest lls)))]))
 
 
 ; Los, Number -> Number
 ; consume list of strings with a number to count
 ; produce the sum of the number and the number of list items
-;(check-expect (aux-line-info line3 0) 6)
-;(check-expect (aux-line-info line2 0) 9)
+;(check-expect (aux-file-info-1string-count-line line3 0) 6)
+;(check-expect (aux-file-info-1string-count-line line2 0) 9)
 ;
-(define (aux-line-info los n)
+(define (aux-file-info-1string-count-line los n)
   (cond
     [(empty? los) n]
-    [else (+ (aux-word-info (explode (first los)) 0)
+    [else (+ (aux-file-info-1string-count-word (explode (first los)) 0)
              (if (empty? (rest los)) 0 1)
-             (aux-line-info (rest los) 0))]))
+             (aux-file-info-1string-count-line (rest los) 0))]))
 
 
 
 ; Los, Number -> Number
 ; consume list of strings with a number to count
 ; produce the sum of the number and the number of list items
-;(check-expect (aux-word-info line0 0) 2)
-;(check-expect (aux-word-info line0 0) 2)
+;(check-expect (aux-file-info-1string-count-word line0 0) 2)
+;(check-expect (aux-file-info-1string-count-word line0 0) 2)
 ;
-(define (aux-word-info los n)
+(define (aux-file-info-1string-count-word los n)
   (cond
     [(empty? los) n]
-    [else (aux-word-info (rest los) (+ n 1))]))
+    [else (aux-file-info-1string-count-word (rest los) (+ n 1))]))
+
+
+; LN -> Number
+;  aux func for file-info for counting words in a input file
+;
+;(check-expect (aux-file-info-word-count lls0) 2)
+;(check-expect (aux-file-info-word-count lls1) 5)
+;(check-expect (aux-file-info-word-count lls3) 7)
+;
+(define (aux-file-info-word-count lls)
+  (cond
+    [(empty? lls) 0]
+    [else (+ (aux-file-info-word-count-line (first lls))
+             (aux-file-info-word-count (rest lls)))]))
+
+
+; Los -> Number
+; aux func for file-info for counting words in a separate text line
+;
+;(check-expect (aux-file-info-word-count-line line0) 2)
+;(check-expect (aux-file-info-word-count-line line1) 0)
+;(check-expect (aux-file-info-word-count-line line2) 5)
+;(check-expect (aux-file-info-word-count-line line3) 2)
+;
+(define (aux-file-info-word-count-line los)
+  (cond
+    [(empty? los) 0]
+    [else (+ 1
+        (aux-file-info-word-count-line (rest los)))]))
+
+
+; LN -> Number
+; aux func for file-info for counting line in a input file
+;
+;(check-expect (aux-file-info-line-count lls0) 2)
+;
+(define (aux-file-info-line-count lls)
+  (cond
+    [(empty? lls) 0]
+    [else (+ 1
+             (aux-file-info-line-count (rest lls)))]))
+
+
 
 
