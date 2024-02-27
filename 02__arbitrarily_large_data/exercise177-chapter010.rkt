00a811e0a0786d85ec0b4133d620dbf31780e676
elsentis <134290921+elsentis@users.noreply.github.com>
2024-02-27 09:23:11 +0300

    Update exercise177-chapter010.rkt

--- 02__arbitrarily_large_data/exercise177-chapter010.rkt
+++ 02__arbitrarily_large_data/exercise177-chapter010.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise177-chapter010) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
 
 ; exercise177-chapter010
 
@@ -8,6 +5,7 @@
 ; Design the function create-editor. The function consumes two strings and produces an Editor.
 ; The first string is the text to the left of the cursor and the second string
 ; is the text to the right of the cursor. The rest of the section relies on this function.
+; (the letters in pre precede the cursor presented in reverse order)
 
 (define-struct editor [pre post])
 ; An Editor is a structure:
@@ -15,6 +13,7 @@
 ; An Lo1S is one of: 
 ; – '()
 ; – (cons 1String Lo1S)
+; (the letters in pre precede the cursor presented in reverse order)
 
 (define good
   (cons "g" (cons "o" (cons "o" (cons "d" '())))))
@@ -33,12 +32,12 @@
 ; String, String -> Editor
 ; consumes two strings and produces an Editor
 ;
-(check-expect (create-editor "all" "good") ed1)
+(check-expect (create-editor "all" "good") ed2)
 (check-expect (create-editor "" "") (make-editor '() '()))
-(check-expect (create-editor "ab" "c") (make-editor (cons "a" (cons "b" '())) (cons "c" '())))
+(check-expect (create-editor "ab" "c") (make-editor (cons "b" (cons "a" '())) (cons "c" '())))
 ;
 (define (create-editor s1 s2)
-  (make-editor (string-li1s s1) (string-li1s s2)))
+  (make-editor (rev (string-li1s s1)) (string-li1s s2)))
 
 ; String -> Lo1S
 ; consume String and produce Lo1S from this input String
@@ -49,4 +48,32 @@
   (cond
     [(<= (string-length s) 0) '()]
     [else (cons (string-ith s 0) (string-li1s (substring s 1)))]))
+
+
+; Lo1s -> Lo1s 
+; produces a reverse version of the given list 
+ 
+(check-expect
+  (rev (cons "a" (cons "b" (cons "c" '()))))
+  (cons "c" (cons "b" (cons "a" '()))))
+ 
+(define (rev l)
+  (cond
+    [(empty? l) '()]
+    [else (add-at-end (rev (rest l)) (first l))]))
+
+
+; Lo1s 1String -> Lo1s
+; creates a new list by adding s to the end of l
+ 
+(check-expect
+  (add-at-end (cons "c" (cons "b" '())) "a")
+  (cons "c" (cons "b" (cons "a" '()))))
+ 
+(define (add-at-end l s)
+  (cond
+    [(empty? l) (cons s '())]
+    [else
+     (cons (first l) (add-at-end (rest l) s))]))
+
                               
